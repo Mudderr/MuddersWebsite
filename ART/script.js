518ecd5a58f6ea8b50653f43d14c7cacfa5c221f
Mud <148325452+Mudderr@users.noreply.github.com>
2024-06-11 09:54:04 +0200

    about me hyperlink

--- ART/script.js
+++ ART/script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var acc = document.getElementsByClassName("collapsible");
-    for (var i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
+    var collapsibles = document.getElementsByClassName("collapsible");
+    var panels = document.getElementsByClassName("panel");
+
+    // Ensure all panels are closed initially
+    for (var j = 0; j < panels.length; j++) {
+        panels[j].style.display = "none";
+    }
+
+    for (var i = 0; i < collapsibles.length; i++) {
+        collapsibles[i].addEventListener("click", function() {
             this.classList.toggle("active");
             var panel = this.nextElementSibling;
             if (panel.style.display === "block") {
